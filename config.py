d9a43532cfc6487620839504cbf5a4498cfcbca2
sage <pm21ms002@iiserkol.ac.in>
2026-08-07 02:00:44 +0530

    added run_validation(), Inference.ipynb, translate.py, attention_visualization.ipynb

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ def get_config():
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
+        "datasource": "Helsinki-NLP/opus_books",
         "lang_src": "en",
         "lang_tgt": "fr",
         "model_folder": "weights",
@@ -17,8 +18,18 @@ def get_config():
     }
 
 def get_weights_file_path(config, epoch: str):
-    model_folder = config['model_folder']
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_basename = config['model_basename']
     model_filename = f"{model_basename}{epoch}.pt"
     return str(Path('.') / model_folder / model_filename)
+
+# Find the latest weights file in the weights folder
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
     
